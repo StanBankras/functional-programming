e44e8e4de79d443a0fa4f9a581596c179240fc95
Stan Bankras <stanbankras@gmail.com>
2020-10-20 11:26:04 +0200

    Bekijken hoeveel van alle parkeerplaatsen betaald zijn

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const axios = require('axios').default;
+const rateLimit = require('axios-rate-limit');
+require('dotenv').config();
+
+const http = rateLimit(axios.create(), { maxRequests: 5, perMilliseconds: 20, maxRPS: 5 });
 
 // Snel even dynamische parkingdata verkennen, ratelimits zitten wel in de weg
 const getParkingFacilities = async () => {
   try {
-    const result = await axios.get('https://npropendata.rdw.nl/parkingdata/v2/');
-    const data = result.data.ParkingFacilities.slice(0, 20); // Slice vanwege ratelimits
+    const result = await http.get(`https://npropendata.rdw.nl/parkingdata/v2?$$app_token=${process.env.OPENDATA_APP_TOKEN}`);
+    const data = result.data.ParkingFacilities;
     let totalTarriffed = 0;
     data.forEach(async (p) => {
-      const query = await axios.get(p.staticDataUrl);
+      const query = await http.get(p.staticDataUrl + '?$$app_token=' + process.env.OPENDATA_APP_TOKEN);
       const data = query.data.parkingFacilityInformation;
       if(!data.tariffs) return console.log('-');
       if(data.tariffs.length === 0) return console.log('-');
