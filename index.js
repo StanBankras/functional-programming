46020a20fe54d39f0df808f6f9c27a0e2aedc237
Stan Bankras <stanbankras@gmail.com>
2020-10-28 12:56:14 +0100

    Remove comments of thoughts on how to retrieve data, now that most data is here

--- index.js
+++ index.js
@@ -59,25 +59,3 @@ function getEnvironmentalZones() {
     return polygons;
   });
 }
-
-/*
-
-Main question:
-Do cities provide extra infrastructure and facilities on parking spots in environment zones to promote (more) green transportation?
-- Are parkingspots closer to public transport to continue your travel this way?
-- Do parkingspots have more charging points for electrical cars?
-- Are parkingspot in environment zones with more charging points more expensive to park?
-
-Code idea:
-1. Gather all parkingspots that have:
-    - Geo data
-    - Amount of charging points
-    - Tariffs
-2. Gather the environment zone coordinates in polygon
-3. Create two different arrays:
-    - Parking spots in environment zones (https://github.com/substack/point-in-polygon)
-    - Parking spots outside environment zones
-4. For both arrays, transform the data so there are arrays of objects with the needed data
-
-
-*/
