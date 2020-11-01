e04f1e2fcb72325f116947d9b1c4f7a6f9388f43
Stan Bankras <stanbankras@gmail.com>
2020-11-01 19:50:02 +0100

    Added loader

--- src/js/d3.js
+++ src/js/d3.js
@@ -1,5 +1,13 @@
+import * as d3 from "d3";
+
+const loader = document.querySelector('#loader');
+const body = document.querySelector('body');
+
 export default function useData(data) {
-  const body = document.querySelector('body');
+  // Clear loader
+  loader.parentNode.removeChild(loader);
+  body.classList.remove('loading');
+  
   const text = document.createElement('p');
   text.textContent = data.length;
   body.appendChild(text);    
